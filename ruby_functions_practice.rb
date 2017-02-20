88d6e2e78e1ac1e525cf47b017521f12a88beaf9
Guy Meikle <left_handed_rocker@hotmail.co.uk>
2017-02-20 21:10:31 +0000

    Tests 1 to 5 pass

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -6,3 +6,28 @@
 	def return_10
 		return 10
 	end
+
+#2
+	def add(num1, num2)
+		num1 + num2
+	end
+
+#3
+	def subtract(num1, num2)
+		num1 - num2
+	end
+
+#4
+	def multiply(num1, num2)
+		num1 * num2
+	end
+
+#5
+	def divide(num1, num2)
+		num1 / num2
+	end
+
+#6
+	def length_of_string
+		test_string.length
+	end
