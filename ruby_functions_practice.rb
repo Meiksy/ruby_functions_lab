141c12ab5cbf91bd9f72433b14067702b38e723c
Guy Meikle <left_handed_rocker@hotmail.co.uk>
2017-02-20 20:57:08 +0000

    Initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,5 @@
+# Use this file to write all your tests. "ruby_fuctions_spec.rb" will aquire this file to run it's tests with.
+
+# You will see in "ruby_fuctions_spec.rb" that all the tests are written for you. The task here is to write all the functions for the tests to bounce off and pass.
+
+#1
